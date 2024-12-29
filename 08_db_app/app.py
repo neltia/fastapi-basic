3e328073cf3e9abf3fb4a26a326eb6b151af1102
Hyunseo <dsz08082@naver.com>
2024-12-29 21:37:55 +0900

    add: api request time logging middleware

--- 08_db_app/app.py
+++ 08_db_app/app.py
@@ -1,6 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from common.error_handler_custom import generic_exception_handler, http_exception_handler
+import time
+import logging
 
 from dotenv import load_dotenv
 from app_mariadb.routes import mariadb_router
@@ -8,12 +10,28 @@ from app_mariadb.routes import mariadb_router
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = FastAPI(title="Database CRUD API", version="1.0")
 app.openapi_version = "3.0.2"
 
 # Custom Error handler
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(Exception, generic_exception_handler)
+
+
+# middleware: API 요청 시간 로깅
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.perf_counter()  # 요청 시작 시간
+    response = await call_next(request)  # 다음 처리로 넘어감
+    end_time = time.perf_counter()  # 요청 종료 시간
+
+    process_time = end_time - start_time
+    logger.info(f"Request: {request.method} {request.url} completed in {process_time:.4f} seconds")
+    return response
+
 
 # Include routers
 app.include_router(mariadb_router, prefix="/mariadb", tags=["MariaDB"])
